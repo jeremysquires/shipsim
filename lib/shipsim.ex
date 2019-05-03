6de23d4979ad6744e8c8fb7d10741642076da96a
Jeremy Squires <jms@mailforce.net>
2019-05-03 16:44:34 -0600

    implement simulator loop

--- lib/shipsim.ex
+++ lib/shipsim.ex
@@ -4,18 +4,17 @@ defmodule ShipSim do
   """
 
   @doc """
-  Initialize the simulator
+  Initialize the simulator using message passing loops
   """
-  def initSimLoop do
+  def init_sim_loop do
     # read json
     file_name = "test/TestData.json"
     # TODO: pattern match against :ok and :error
     {_result, vessels} = ShipSim.JSONFetch.fetch(file_name)
-    # TODO: preprocess all timestamp strings to DateTime objects
     # start time slice at the lowest time
     # TODO: find the lowest timestamp
     lowest_time = "2020-01-01T07:40Z"
-    # spawn ship message listeners
+    # TODO: spawn ship message listeners using ShipLoop for message passing
     Enum.map(vessels,
       fn vessel ->
         spawn ShipSim.ShipLoop.ops_loop(%{
@@ -28,14 +27,74 @@ defmodule ShipSim do
   end
 
   @doc """
+  Initialize the simulator using ShipAgent processes
+  """
+  def init_sim_agents do
+    # read json
+    file_name = "test/TestData.json"
+    # TODO: pattern match against :ok and :error
+    {_result, _vessels} = ShipSim.JSONFetch.fetch(file_name)
+    # start time slice at the lowest time
+    # TODO: find the lowest timestamp
+    _lowest_time = "2020-01-01T07:40Z"
+  end
+
+  def advance_loop(ship_trackers, timestamp, highest_time, closest_points) when timestamp > highest_time do
+    {:ok, ship_trackers, closest_points}
+  end
+
+  def advance_loop(ship_trackers, _timestamp, highest_time, closest_points) do
+    new_ship_trackers = Enum.map(ship_trackers,
+      fn tracker ->
+        ShipSim.Ship.advance(tracker, 60)
+      end
+    )
+    # find the closest points of approach
+    # TODO: | 1 => %{position: position, range: range, bearing: bearing}
+    new_closest_points = closest_points
+    new_timestamp = List.first(new_ship_trackers)[:current_time]
+    advance_loop(new_ship_trackers, new_timestamp, highest_time, new_closest_points)
+  end
+
+  @doc """
   Run the simulator
   """
-  def runSim do
+  def run_sim do
+    file_name = "test/TestData.json"
+    # TODO: pattern match against :ok and :error
+    {:ok, vessels} = ShipSim.JSONFetch.fetch(file_name)
+    # start time slice at the lowest time
+    # TODO: find the lowest timestamp
+    lowest_time = "2020-01-01T07:40Z"
+    # end at the highest time
+    # TODO: find the highest timestamp
+    highest_time = "2020-01-01T11:24Z"
     # initialize all ship processes
-    _ships = ShipSim.initSimLoop()
+    # using message passing loop
+    # _ships = ShipSim.initSimLoop()
+    # using Agent
+    # using Task
+    # using in memory objects
+    ships = vessels["vessels"]
+    ship_trackers = Enum.map(ships,
+      fn ship ->
+        start_position = List.first(ship["positions"])
+        ship_tracker =
+          Map.put(ship, :position_index, 0) |>
+          Map.put(:current_time, lowest_time) |>
+          Map.put(:current_position, start_position)
+        ShipSim.Ship.where(ship_tracker, lowest_time)
+      end
+    )
     # move time slice ahead in loop
-    # notify all registered ships
     # until latest time is encountered
+    {:ok, end_trackers, closest_points} = advance_loop(ship_trackers, lowest_time, highest_time, %{})
+    IO.puts "#{inspect end_trackers}"
+    IO.puts "#{inspect closest_points}"
+    # output closest point of approach information
+  
+    # output distance and speed for all ships
+    _runs = ShipSim.DaysRun.days_run_out(vessels)
   end
 
 end
