f1b6a17e0962cf65134e56bea94cddb9c48e7f9c
Jeremy Squires <jms@mailforce.net>
2019-06-05 19:33:15 -0600

    parallel days run via tasks

--- lib/shipsim/days_run.ex
+++ lib/shipsim/days_run.ex
@@ -1,36 +1,60 @@
 defmodule ShipSim.DaysRun do
   def days_run(vessels) do
     # TODO: calculate days run in parallel
-    Enum.map(
+    tasks = Enum.map(
       vessels["vessels"],
       fn vessel ->
-        %{"name" => vesselname, "positions" => positions} = vessel
-        [firstposition|lastpositions] = positions
-        %{"timestamp" => firsttime, "x" => firstx, "y" => firsty} = firstposition
-        full_days_run = Enum.reduce(lastpositions,
-          {0, {firstx,firsty}, 0, firsttime},
-          fn position, acc ->
-            %{"timestamp" => timestamp, "x" => x, "y" => y} = position
-            {last_distance, {lastx, lasty}, last_delta, last_time} = acc
-            new_distance = last_distance + Segment.length(lastx, lasty, x, y)
-            new_delta = last_delta + TimeStamp.delta_time(last_time, timestamp)
-            {new_distance, {x,y}, new_delta, timestamp}
+        Task.async(
+          fn ->
+            %{"name" => vesselname, "positions" => positions} = vessel
+            [firstposition|lastpositions] = positions
+            %{"timestamp" => firsttime, "x" => firstx, "y" => firsty} = firstposition
+            full_days_run = Enum.reduce(lastpositions,
+              {0, {firstx,firsty}, 0, firsttime},
+              fn position, acc ->
+                %{"timestamp" => timestamp, "x" => x, "y" => y} = position
+                {last_distance, {lastx, lasty}, last_delta, last_time} = acc
+                new_distance = last_distance + Segment.length(lastx, lasty, x, y)
+                new_delta = last_delta + TimeStamp.delta_time(last_time, timestamp)
+                {new_distance, {x,y}, new_delta, timestamp}
+              end
+            )
+            distance_run = elem(full_days_run, 0)
+            hours_run = elem(full_days_run, 2) / 60 / 60
+            speed =
+              if (hours_run == 0) do
+                0.0
+              else
+            distance_run / hours_run
+              end
+            %{
+              :vesselname => vesselname,
+              :distance_run => distance_run,
+              :hours_run => hours_run,
+              :speed => speed
+            }
           end
         )
-        distance_run = elem(full_days_run, 0)
-        hours_run = elem(full_days_run, 2) / 60 / 60
-        speed =
-          if (hours_run == 0) do
-            0.0
-          else
-		    distance_run / hours_run
-          end
-        %{
-          :vesselname => vesselname,
-          :distance_run => distance_run,
-          :hours_run => hours_run,
-          :speed => speed
-        }
+      end
+    )
+    completedTasks = Task.yield_many(tasks, :infinity)
+    results = Enum.map(
+      completedTasks,
+      fn { task, result } ->
+        result || Task.shutdown(task, :brutal_kill)
+      end
+    )
+    Enum.map(
+      results,
+      fn result ->
+        case result do
+          { :ok, res } ->
+            res
+          { :exit, reason } ->
+            %{ distance_run: 0.0, hours_run: 0.0, speed: 0.0, vesselname: reason }
+          _ ->
+            %{ distance_run: 0.0, hours_run: 0.0, speed: 0.0, vesselname: "unknown error" }
+        end
       end
     )
   end  
